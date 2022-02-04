d8af5aa81b019b570ce09e2fc3b1ad6bbd4f98b0
Kai <98994629+FireOVA@users.noreply.github.com>
2022-02-04 15:26:23 -0700

    Add files via upload

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(75, 61);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(168, 56);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "A";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.Switch_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(75, 147);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(168, 58);
+            this.btn2.TabIndex = 1;
+            this.btn2.Text = "B";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.Switch_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(75, 230);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(167, 62);
+            this.btn3.TabIndex = 2;
+            this.btn3.Text = "C";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.Switch_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(350, 406);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Switch_Click(object sender, EventArgs e)
+        {
+            (string, string, string) p = (btn1.Name, btn2.Name, btn3.Name);
+            string cType = p;
+            switch (cType)
+            {
+                case btn1:
+                    
+                    break;
+                case btn2:
+                    
+                    break;
+                case btn3:
+                    
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
